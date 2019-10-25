40040a6ca17340080de463da6bee3e660f61450d
Alexandre Lages <alexandrelages64@gmail.com>
2019-10-25 13:53:11 -0300

    Primeiro commit - configurando o projeto

---
+++ src/main/java/br/com/infoway/BancoInfowayApplication.java
@@ -0,0 +1,13 @@
+package br.com.infoway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoInfowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoInfowayApplication.class, args);
+	}
+
+}
